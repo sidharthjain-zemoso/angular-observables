45cd09b0b78b48c7b76e3baed3d5195e7700c440
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-11 10:49:31 +0530

    errors and completion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,13 +19,31 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count == 3) {
+          // halting the observable
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error("Count is greater than 3!"));
+        }
         count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
-      console.log(data);
-    });
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        // handling errors
+        // console.log(error);
+        console.log(error.message);
+      },
+      () => {
+        //clean up
+        console.log("completed");
+      }
+    );
   }
 
   ngOnDestroy(): void {
